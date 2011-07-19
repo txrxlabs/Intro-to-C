d982c1c4b5fc8bbf4de4dbb47fe7052fd048761d
Stephen M. Cameron <smcameron@yahoo.com>
2011-07-19 07:53:46 -0500

    assignment-statements

---
+++ examples/0004-assignment-statement.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+
+	printf("Hello, world!\n");
+
+	/* straight forward assignments */
+	a = 1;
+	b = 2;
+	c = a + b;
+	printf("a = %d, b= %d, c = %d\n", a, b, c);
+
+	/* b is assigned value of c, then a assigned the value of b 
+	 * (same as c) 
+	 */
+	a = b = c;
+
+	/* Same as: a = a + 1; */
+	a += 1;
+
+	/* Same as: b = b - 5; */`
+	b -= 5;
+
+	/* Almost same as c = c + 1;  */
+	c++;
+
+	/* Same as: c = c * 2 */
+	c *= 2;
+
+	/* Almost same as c = c - 1; */
+	c--;
+
+	/* Almost same as c = c - 1; */
+	--c;
+
+	printf("a = %d, b= %d, c = %d\n", a, b, c);
+
+	return 0;
+}
+
